fbe517c816b044484c06e5d004d4c4fa54f1b2a7
liukaiming910 <9549717+liukaiming910@user.noreply.gitee.com>
2024-11-03 17:04:45 +0800

    feat: doc page

---
+++ doc/docusaurus.config.ts
@@ -0,0 +1,67 @@
+import type * as Preset from '@docusaurus/preset-classic';
+import type { Config } from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+
+const config: Config = {
+  title: 'Dream family 文档',
+  tagline: '个人学习demo记录记录',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/dream-family',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          id: 'zh',
+          path: './docs/doc_ZH',
+          sidebarPath: './sidebars.ts',
+          routeBasePath: 'zh',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  plugins: [
+    [
+      'content-docs',
+      {
+        id: 'en',
+        path: './docs/doc_EN',
+        sidebarPath: './sidebars.ts',
+        routeBasePath: 'en',
+      },
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
